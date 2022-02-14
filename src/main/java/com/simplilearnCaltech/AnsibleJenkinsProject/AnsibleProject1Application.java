b8cb7ae9c60beb1215e51a7a0a32887cbf7f0181
Tina kamali-Anaraki <tina.kamali15@gmail.com>
2022-02-14 14:09:41 -0500

    recover commit

---
+++ src/main/java/com/simplilearnCaltech/AnsibleJenkinsProject/AnsibleProject1Application.java
@@ -0,0 +1,13 @@
+package com.simplilearnCaltech.AnsibleJenkinsProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnsibleProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnsibleProject1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplilearnCaltech/AnsibleJenkinsProject/AnsibleProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearnCaltech.AnsibleJenkinsProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnsibleProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
